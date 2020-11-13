acabc805346cb228b4c4d25841d40fce202d50c6
Miah <revolution5117@gmail.com>
2020-11-13 00:02:14 -0800

    Issues getting usercontroller test to work, problems in user contorller

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/config/DataSourceConfig.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/config/DataSourceConfig.java
@@ -3,6 +3,7 @@ package com.lambdaschool.shoppingcart.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,6 +29,14 @@ public class DataSourceConfig {
             String myURLString = "Jdbc:h2:mem:testdb";
             String myDriverClass = "org.h2.Driver";
             String mydbUser = "sa";
+            String mydbPassword = "";
+
+            return DataSourceBuilder.create()
+                    .username(mydbUser)
+                    .password(mydbPassword)
+                    .url(myURLString)
+                    .driverClassName(myDriverClass)
+                    .build();
 
         }
     }
